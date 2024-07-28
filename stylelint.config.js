36141e973e72a07ea74b405e16fe12cc0c8e4ea7
Robin Bisping <robin@bisping.ch>
2024-07-28 17:24:47 +0200

    Update stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,8 @@
+export default {
+  extends: ["stylelint-config-standard-scss", "stylelint-config-recess-order"],
+  rules: {
+    "no-descending-specificity": null,
+    "selector-class-pattern": null,
+    "selector-id-pattern": null,
+  },
+};
